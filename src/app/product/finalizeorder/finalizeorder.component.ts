9bf7c75b2c17786c16da1e3aa7ef25abc97ec31f
Andrey689p <andrey.bertolettiteste@gmail.com>
2024-11-11 18:11:38 -0300

    Atualização

--- src/app/product/finalizeorder/finalizeorder.component.ts
+++ src/app/product/finalizeorder/finalizeorder.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-finalizeorder',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './finalizeorder.component.html',
   styleUrl: './finalizeorder.component.css'
 })
 export class FinalizeorderComponent {
+selectedPaymentMethod: string | null = null;
 
+  // Método para alternar a seleção do método de pagamento
+  selectPaymentMethod(method: string) {
+    this.selectedPaymentMethod = method;
+  }
 }
